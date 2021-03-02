e858e2b76552122cb1d94ac54f07713a32f50a56
yatsun <yatsunlam25@gmail.com>
2021-03-02 00:38:58 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words) # put argument(s) here
+    i = 0
+    temp = []
+    if words.size > i
+        while i < words.size
+            words.collect do |word|
+                yield(word)
+                temp << word
+                i += 1
+            end
+        end
+    else
+       # "This block should not run!"
+    end
+    temp
+    
 end
